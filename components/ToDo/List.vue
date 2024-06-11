39069f103b3b88cafc747facc336fba24fc21365
salwa mazarin <tech_dev4@indicocompany.com>
2024-06-11 14:48:42 +0700

    follow up technical test

--- components/ToDo/List.vue
+++ components/ToDo/List.vue
@@ -1,24 +1,23 @@
 <template>
   <div class="fluid">
-    <label>Task</label>
-    <InputText v-model="taskDescription"></InputText>
-    <div class="fluid grid">
-      <label> Duration </label>
-      <InputText v-model="durationTask"></InputText>
-      <Dropdown
-        v-model="selectedDescDuration"
-        :options="descriptionDuration"
-        optionLabel="name"
-      ></Dropdown>
-      <Button @click="createTask()" label="Add New Task"></Button>
+    <div class="create-box">
+      <label>Task</label>
+      <InputText v-model="taskDescription"></InputText>
+      <div class="fluid grid">
+        <label> Duration </label>
+        <InputText v-model="durationTask"></InputText>
+        <Dropdown
+          v-model="selectedDescDuration"
+          :options="descriptionDuration"
+          optionLabel="name"
+        ></Dropdown>
+        <Button class="btn-create" @click="createTask()" label="Add New Task"></Button>
+      </div>
     </div>
   </div>
-  <div class="fluid">
-    <Checkbox v-model="active">
-    </Checkbox>
-    <label>
-      Active
-    </label>
+  <div class="fluid filter">
+    <Checkbox v-model="active" value="active" binary @click="filterActive()"> </Checkbox>
+    <label> Active </label>
   </div>
   <div>
     <DataTable :value="taskList" tableStyle="min-width: 50rem">
@@ -38,19 +37,34 @@
           </div></template
         >
       </Column>
-      <Column field="duration" header="Duration"> </Column>
+      <Column field="duration" header="Duration"> 
+        <template #body="slotProps">
+          <div v-if="editable">
+            <InputText
+              v-model="slotProps.data.duration"
+              :value="slotProps.data.duration"
+            ></InputText>
+          </div>
+          <div v-else>
+            <span>
+              {{ slotProps.data.duration }}
+            </span>
+          </div></template
+        >
+      </Column>
       <Column field="deletedAt" header="Deleted At"></Column>
       <Column header="Action">
         <template #body="slotProps">
-          <div v-if="!editable">
+          <div v-if="!editable" class="btn-action">
             <Button label="Edit" @click="editText(slotProps.data.id)"></Button>
             <Button
               label="Soft Delete"
-              @click="deleteTask(slotProps.data.id)"
+              @click="deleteTask(slotProps.data.id, false)"
+            
             ></Button>
             <Button
               label="Hard Delete"
-              @click="deleteTask(slotProps.data.id)"
+              @click="deleteTask(slotProps.data.id, true)"
             ></Button>
           </div>
           <div v-else>
@@ -68,6 +82,7 @@
 const taskList = ref();
 const taskDescription = ref();
 const durationTask = ref();
+const active = ref(false);
 const editable = ref(false);
 const taskDescriptionList = ref();
 const descriptionDuration = ref([
@@ -91,30 +106,89 @@ async function loadData() {
     console.error("Cannot Access Server");
   }
 }
+async function filterActive() {
+  try {
+    console.log(active.value)
+    const response = await $fetch(`/api/todos?active=${!active.value}`);
+    taskList.value = response;
+  } catch (error) {
+    console.error("Cannot Access Server");
+  }
+}
 async function createTask() {
   // TODO : Implement Create Task to Backend
+  try {
+    const newData = {
+    task: taskDescription.value,
+    duration: durationTask.value + selectedDescDuration.value.code
+  }
+    const response = await $fetch(`/api/todos`, {
+        method: 'POST',
+        body: newData
+    });
+    taskList.value = response;
+  } catch (error) {
+    console.error("Cannot Access Server");
+  }
 }
 async function editText() {
   editable.value = true;
 }
 async function cancelEdit() {
   editable.value = false;
+  loadData()
 }
 async function updateTask(data) {
   // TODO : Implement Update Task to Backend
+  console.log(data.task, "data pdate")
+  try {
+    const newData = {
+    task: data.task,
+    duration: data.duration
+  }
+    const response = await $fetch(`/api/todos/${data.id}`, {
+        method: 'PATCH',
+        body: newData
+    });
+    taskList.value = response;
+  editable.value = false;
+
+  } catch (error) {
+    console.error("Cannot Access Server");
+  }
 }
-async function deleteTask(id) {
+async function deleteTask(id, hard) {
   // TODO : Implement Condition Hard Delete
   try {
-    const response = await $fetch(`/api/todos/${id}`, {
+    const response = await $fetch(`/api/todos/${id}?hard=${hard}`, {
       method: "DELETE",
       body: [],
     });
-    return response;
+    taskList.value =  response;
+
   } catch (error) {
     console.error(error);
   }
 }
 </script>
 
-<style></style>
+<style>
+.create-box {
+  display: flex;
+  align-items: center;
+  margin: 24px 0;
+}
+label {
+  margin: 0 12px;
+}
+.btn-create{
+  margin-left: 24px;
+}
+.filter{
+  margin: 24px 0;
+}
+.btn-action{
+  display: flex;
+  gap: 12px
+}
+</style>
